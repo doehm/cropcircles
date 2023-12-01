94bde278089fa154e05c1bd0996fb48b362dc204
Daniel Oehm <danieloehm@gmail.com>
2023-12-01 18:44:55 +1100

    DOC updating doco

--- R/crop.R
+++ R/crop.R
@@ -15,6 +15,11 @@
 #' @param bg_fill Background fill. Allows a different colour for the background and
 #' a different colour for the border.
 #'
+#' @note
+#' The naming convention is now `crop_*`. The old functions
+#' `circle_crop` and `hex_crop` still work but you are encouraged to use the new
+#' functions `crop_circle` and `crop_hex`.
+#'
 #' @importFrom magick image_read image_data image_write image_crop image_resize image_blank image_info image_composite image_flip
 #' @importFrom glue glue
 #'
@@ -201,61 +206,10 @@ crop_parallelogram <- function(images, to = NULL, border_size = NULL, border_col
 
 }
 
-
-#' Cropping functions
-#'
-#' [deprecated] The naming convention is now `crop_*`. The old functions
-#' `circle_crop` and `hex_crop` still work but you are encouraged to use the new
-#' functions `crop_circle` and `crop_hex`. There is no difference between these
-#' functions other than the name - Reads in an image and crops to the specified geometry with a transparent
-#' background. If a new path is given it will save the cropped images to
-#' the new location. If no path is given it will save to a temporary location
-#' which will be cleared when the session is closed
-#'
-#' @param images Vector of image paths, either local or urls. If urls the images
-#' will be downloaded first.
-#' @param to Path to new location
-#' @param border_size Border size in pixels.
-#' @param border_colour Border colour.
-#' @param just Where to justify the image prior to cropping. Accepted values:
-#' `left`, `right`, `top`, `bottom`
-#' @param bg_fill Background fill. Allows a different colour for the background and
-#' a different colour for the border.
-#'
-#' @importFrom magick image_read image_data image_write image_crop image_resize image_blank image_info image_composite image_flip
-#' @importFrom glue glue
-#'
-#' @name crop
-#'
-#' @return Path to cropped images
+#' @rdname crop_circle
 #' @export
-#'
-#' @examples
-#' library(cropcircles)
-#' library(magick)
-#'
-#' img_path <- file.path(system.file(package = "cropcircles"), "images", "walter-jesse.png")
-#' img_cropped <- crop_circle(img_path, border_size = 6)
-#' image_read(img_cropped)
-#'
-#' # other geometries
-#'
-#' image_read(crop_hex(img_path, border_size = 6))
-#' image_read(crop_heart(img_path, border_size = 6))
-#' image_read(crop_parallelogram(img_path, border_size = 6))
-#'
-#' # justification example
-#'
-#' # center (default)
-#' image_read(crop_circle(img_path, border_size = 6))
-#'
-#' # left
-#' image_read(crop_circle(img_path, border_size = 6, just = "left"))
-#'
-#' # right
-#' image_read(crop_circle(img_path, border_size = 6, just = "right"))
 circle_crop <- crop_circle
 
-#' @rdname crop
+#' @rdname crop_circle
 #' @export
 hex_crop <- crop_hex
